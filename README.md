ae75bc5824f89784946c13fd8769203db3e61924
BBK-PiJ-2014-33 <ludak303@gmail.com>
2015-04-26 20:50:48 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 A web crawler is program that follows links on web pages on its own
+The database used for storing two tables is MySQL database. This is one of the open source databases.
+Introduction to MySQL can be found at http://code.tutsplus.com/tutorials/everything-you-need-to-get-started-with-mysql--net-3076
+
--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -1,11 +1,14 @@
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class WebCrawler implements WebCrawlerInterface {
     //maximum number of distinct links crawler can process
     //defaults to 10
-    private int maxLinks = 10;
+    private static final int maxLinks = 10;
     //maximum depth of search (priority)
     //defaults to 2
-    private int maxDepth = 2;
+    private static final int maxDepth = 2;
     public WebCrawler(){
 
     }
@@ -13,9 +16,20 @@ public class WebCrawler implements WebCrawlerInterface {
     //save all URL links found within the page to a temporary database table
     //database table contains two columns which are: * column containing complete URLS with base references appended as needed
     //                                               * priority queue number
+    //process until either:
+    //                      number of distinct links has been exhausted
+    //                      all links related to the maximum depth (priority number) have been processed
+    //                      there are no links remaining to be processed
     public void crawl(String url, String database)
     {
-
+        //open an HTTP connection to the starting URL
+        try{
+            URL myUrl = new URL(url);
+            HttpURLConnection myConnection = (HttpURLConnection) myUrl.openConnection();
+            readStream(myConnection.getInputStream());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
     public int search (String url)
     {
@@ -26,6 +40,10 @@ public class WebCrawler implements WebCrawlerInterface {
 
     }
     private void setMaxDepth(int myDepth)
+    {
+
+    }
+    private void readStream(InputStream myInputStream)
     {
 
     }
